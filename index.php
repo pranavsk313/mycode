74dc42f910ec3d21eb2bf2b0a4c04650071a8856
pranavsk313 <122976840+pranavsk313@users.noreply.github.com>
2023-01-26 01:32:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My first PHP page using OpenShift S2I</h1>
+<h1>My first PHP page using OpenShift S2I automated by webhook </h1>
   <h2> This is new updated php page </h2>
 <?php
 echo "Hello from pranav";
